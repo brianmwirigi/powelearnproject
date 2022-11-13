c671a7dc514dd9022ba63ff5a93d9067037726bc
mwirigi <bmmaingi17@gmail.com>
2022-11-14 00:01:11 +0300

    javascript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let myName = "Brian" //variable declared as string data type
+let myName = "Brian" //let leyword used to asign variable declared as string data type
 let Age = 22 //varable declared as an integer data type
 
 let married = false //variable declareed as boolean data type
 
+var gender = "male" //variable keyword can be used to declare variable
+
+const educationLevel = "Post graduate" // constant keyword used to declare variables that cannot be changed
