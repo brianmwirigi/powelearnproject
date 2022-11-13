85786e85279a875b790b14cfc2564260a8d68963
mwirigi <bmmaingi17@gmail.com>
2022-11-13 23:05:29 +0300

    variables

---
+++ script.js
@@ -0,0 +1,5 @@
+let myName = "Brian" //variable declared as string data type
+let Age = 22 //varable declared as an integer data type
+
+let married = false //variable declareed as boolean data type
+
